ef5e65cb637ed2046aed05007430cfbb9df2a54e
moonrailgun <moonrailgun@gmail.com>
2017-03-28 08:47:38 +0800

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -6,28 +6,28 @@ var requireDir = require('require-dir');
 requireDir('./gulp/tasks', { recurse: true});
 
 gulp.task('default', ['clean'], function(){
-  gulp.start('html', 'assets', 'webpack');
+  gulp.start('sass', 'html', 'assets', 'webpack');
 });
 
 gulp.task('watch', gulpsync.sync(
   [
     'clean',
-    ['html', 'assets', 'webpack']
+    ['sass', 'html', 'assets', 'webpack']
   ]),function(){
-  // watch(config.sass.all, function(){  //监听所有sass
-  //   gulp.start('sass');             //出现修改、立马执行sass任务
-  // });
+    watch(config.sass.all, function(){  //监听所有sass
+      gulp.start('sass');             //出现修改、立马执行sass任务
+    });
 
-  watch(config.html.src, function(){
-    gulp.start('html');
-  });
+    watch(config.html.src, function(){
+      gulp.start('html');
+    });
 
-  watch(config.assets.src, function(){
-    gulp.start('assets');
-  });
+    watch(config.assets.src, function(){
+      gulp.start('assets');
+    });
 
-  watch([config.base.src + '/*.js', config.base.src + '/**/*.js'], function(){  //监听所有 es6 js
-    gulp.start('webpack');
-  });
-}
+    watch([config.base.src + '/*.js', config.base.src + '/**/*.js'], function(){  //监听所有 es6 js
+      gulp.start('webpack');
+    });
+  }
 );
